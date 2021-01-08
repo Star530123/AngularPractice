d4dba4104738083f419cc09a61b7a73d4e3ba594
YuXing <simon.lin@tpisoftware.com>
2021-01-08 15:56:20 +0800

    feat : Tutorial 3 Add services

---
+++ src/app/hero.service.ts
@@ -0,0 +1,18 @@
+import { Hero } from './hero';
+import { Injectable } from '@angular/core';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+
+  constructor(private messageService: MessageService) { }
+}
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,31 @@
+import { MessageService } from './../message.service';
+import { Hero } from './../hero';
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
+
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Seletcted hero id=${hero.id}`);
+  }
+}
